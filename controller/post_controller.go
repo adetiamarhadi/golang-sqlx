ae42830da6353788ae1c3f489257343ac3f4ebe4
adetiamarhadi <adetia.marhadi@gmail.com>
2022-02-01 10:29:05 +0700

    sqlx select and get

--- controller/post_controller.go
+++ controller/post_controller.go
@@ -1,7 +1,6 @@
 package controller
 
 import (
-	"database/sql"
 	"net/http"
 	"strconv"
 	"time"
@@ -14,7 +13,7 @@ type Post struct {
 	ID        int64 `json:"id"`
 	Title     string `json:"title"`
 	Content   string `json:"content"`
-	CreatedAt time.Time `json:"created_at"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
 }
 
 func CreatePost(c *gin.Context) {
@@ -40,25 +39,7 @@ func CreatePost(c *gin.Context) {
 func GetPosts(c *gin.Context) {
 	var posts []Post
 
-	rows, err := dbclient.DBClient.Query("SELECT id, title, content, created_at FROM posts")
-	if err != nil {
-		c.JSON(http.StatusUnprocessableEntity, gin.H{
-			"error": true,
-		})
-		return
-	}
-
-	for rows.Next() {
-		var post Post
-		if err := rows.Scan(&post.ID, &post.Title, &post.Content, &post.CreatedAt); err != nil {
-			c.JSON(http.StatusUnprocessableEntity, gin.H{
-				"error": true,
-			})
-			return
-		}
-
-		posts = append(posts, post)
-	}
+	dbclient.DBClient.Select(&posts, "SELECT id, title, content, created_at FROM posts")
 
 	c.JSON(http.StatusOK, posts)
 }
@@ -67,21 +48,9 @@ func GetPost(c *gin.Context) {
 	idStr := c.Param("id")
 	id, _ := strconv.Atoi(idStr)
 
-	row := dbclient.DBClient.QueryRow("SELECT id, title, content, created_at FROM posts WHERE id = ?", id)
-
 	var post Post
-	if err := row.Scan(&post.ID, &post.Title, &post.Content, &post.CreatedAt); err != nil {
-		if err == sql.ErrNoRows {
-			c.JSON(http.StatusNotFound, gin.H{
-				"error": true,
-			})
-			return
-		}
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": true,
-		})
-		return
-	}
+
+	dbclient.DBClient.Get(&post, "SELECT id, title, content, created_at FROM posts WHERE id = ?", id)
 
 	c.JSON(http.StatusOK, post)
 }
